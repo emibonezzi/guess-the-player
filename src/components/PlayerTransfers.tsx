fe459de796286713e42ba146b76565129c29bec6
emibonezzi <emibonezzi@gmail.com>
2024-03-07 14:53:08 -0500

    Fix transfer not happened yet

--- src/components/PlayerTransfers.tsx
+++ src/components/PlayerTransfers.tsx
@@ -3,6 +3,7 @@ import useTeam from "../hooks/useTeam";
 import TeamCard from "./TeamCard";
 import useFilterQueryStore from "../state-management/filter-query/store";
 import getRandomTeamId from "../utils/getRandomTeamId";
+import test from "node:test";
 
 const PlayerTransfers = () => {
   const { randomPlayer, isLoadingPlayer } = useTeam();
@@ -18,12 +19,13 @@ const PlayerTransfers = () => {
     <>
       <div className="d-flex gap-2 flex-wrap">
         <Divider />
-        {/* if first transfer, switch in and out */}
+        {/* if id is Sparta don't consider transfer, if first transfer, switch in and out, if last transfer year is current dont considere */}
         {transfers
           ?.filter((item) => item.teams.in.id !== 20674)
           .map((t, i) =>
             i === 0 ? { ...t, teams: { ...t.teams, in: t.teams.out } } : t
           )
+          .filter((item) => item.type !== "N/A")
           .map((transfer, index) => (
             <TeamCard key={index} type="in" transfer={transfer} />
           ))}
