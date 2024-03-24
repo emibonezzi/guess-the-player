dbde4f8587efbc959e6a0d018e2c3fb8d22c6ee0
emibonezzi <emibonezzi@gmail.com>
2024-03-24 10:43:00 -0400

    Add filters layout

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Image,
+  Select,
+} from "@chakra-ui/react";
 import "@fontsource-variable/inter-tight";
 import "@fontsource-variable/noto-sans-armenian";
 import "@fontsource/protest-strike";
@@ -14,7 +22,7 @@ const Homepage = () => {
 
   return (
     <Box
-      mt="200px"
+      mt="60px"
       display="flex"
       flexDirection="column"
       alignItems="center"
@@ -29,15 +37,9 @@ const Homepage = () => {
           stiffness: 150,
           damping: 20,
         }}
-        className="d-flex flex-column gap-3 align-items-center "
+        className="d-flex flex-column gap-3 align-items-center"
       >
-        <Box
-          w="250px"
-          position="relative"
-          boxShadow="xl"
-          borderRadius={50}
-          p={3}
-        >
+        <Box w="250px" position="relative" borderRadius={50} p={3}>
           <Box
             fontSize="40px"
             position="absolute"
@@ -58,7 +60,27 @@ const Homepage = () => {
           </Box>
           <Image src={logo} />
         </Box>
+
+        <FormControl w="280px">
+          <FormLabel>League: </FormLabel>
+          <Select>
+            <option value=""></option>
+          </Select>
+          <FormHelperText>
+            Up for a challenge? Select all leagues.
+          </FormHelperText>
+        </FormControl>
+        <FormControl w="280px">
+          <FormLabel>Level: </FormLabel>
+          <Select>
+            <option value=""></option>
+          </Select>
+          <FormHelperText>
+            The extreme mode will unleash obscure players.
+          </FormHelperText>
+        </FormControl>
         <Button
+          mt={3}
           onClick={() => {
             refetchPlayer();
             navigate("/game");
