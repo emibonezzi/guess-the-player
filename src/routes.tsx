4bad1b3a98e3fd2fbe790da10498462230ed6c62
Emi <111168037+emibonezzi@users.noreply.github.com>
2024-04-26 13:29:15 -0400

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import Homepage from "./components/Homepage";
+import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <Homepage /> }],
+    children: [{ index: true, element: <Homepage /> }, { path: "game", element: <App /> }],
   },
 ]);
 
